768c246954b6b1536fab58399be74d55e4cd51f0
Nguyễn Nhất Sang <nguyennhatsang2002@gmail.com>
2025-03-26 15:42:54 +0700

    video 29 useState, Data type

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  // data-types
+
+  // string
+  const [name, setName] = useState<string>("Nhat Sang");
+
+  // number
+  const [age, setAge] = useState<number>(23);
+
+  // null , undefine, boolean
+  const test = null;
+
+  // object, array
+  const [person, serPerson] = useState({
+    name: "Sang",
+    age: 23,
+  });
+
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.text} >Test</Text>
+        <Text style={styles.text} >{name}</Text>
+        <Text style={styles.text} >{age}</Text>
+        <Text style={styles.text} >{JSON.stringify(person)}</Text>
       </View>
       <Text>Hello
         <Text style={styles.worldtext}> World!!!</Text>
@@ -25,7 +46,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    fontSize: 60,
+    color: 'red',
+    paddingTop: 40,
+    paddingHorizontal: 20,
   },
 });
