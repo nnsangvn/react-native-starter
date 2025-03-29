95114de67ee1c57037715170a7f50f46bfd86366
Nguyễn Nhất Sang <nguyennhatsang2002@gmail.com>
2025-03-29 09:06:12 +0700

    Hook: useRoute - passing params to routes

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Button, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native';
 
 function App() {
   const Stack = createNativeStackNavigator();
@@ -20,12 +21,12 @@ function App() {
         <View style={{ marginBottom: 10 }}>
           <Button
             title='Go user 1'
-            onPress={() => navigation.navigate('Details')} />
+            onPress={() => navigation.navigate('Details', { userId: 1, name: "Sang" })} />
         </View>
         <View style={{ marginBottom: 10 }}>
           <Button
             title='Go user 2'
-            onPress={() => navigation.navigate('Details')} />
+            onPress={() => navigation.navigate('Details', { userId: 2, name: "Nguyen" })} />
         </View>
       </View>
     );
@@ -33,10 +34,13 @@ function App() {
 
   function DetailsScreen() {
     const navigation: any = useNavigation();
+    const route: any = useRoute();
+    console.log(">>Check route:", route);
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
+        <Text>Details Screen </Text>
+        <Text>User Id = {route?.params?.userId}</Text>
         <Button
           title='Back to Home'
           onPress={() => navigation.goBack()} />
